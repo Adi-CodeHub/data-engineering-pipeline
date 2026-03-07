cbc1ad8fd99ca7f36bfd9ed35cad3bfc2066bafb
AdiCodeHub <study.aditya.sha@gmail.com>
2026-03-07 23:07:50 +0530

    ETL pipeline code

---
+++ app/processing/etl_pipeline.py
@@ -0,0 +1,72 @@
+from app.ingestion.api_client import APIClient
+from app.processing.validator import DataValidator
+from app.processing.cleaner import DataCleaner
+from app.processing.transformer import DataTransformer
+from app.storage.database import DatabaseManager
+from app.utils.logger import get_logger
+import glob 
+import os
+
+logger = get_logger(__name__)
+
+class ETLPipeline:
+
+    def __init__(self, api_url:str):
+        self.api_url = api_url
+
+    def extract(self):
+        logger.info("Starting Extraction Step")
+
+        client = APIClient(self.api_url)
+        data = client.fetch_data()
+        client.save_raw_data(data)
+
+        logger.info("Extraction completed")
+
+    def validate(self):
+        logger.info("Starting Data Validation")
+
+        latest_file = max(glob.glob("data/raw/*json"), key=os.path.getctime)
+        validator = DataValidator(latest_file)
+        validator.load_raw_data()
+        validator.convert_to_dataframe()
+        validator.generate_schema_report()
+
+        logger.info("Validation completed")
+        return self.df
+    
+    def transform(self, df):
+
+        logger.info("Starting Data Transformation")
+
+        cleaner = DataCleaner(df)
+        clean_df = cleaner.clean()
+
+        transformer = DataTransformer(clean_df)
+        countries, regions, languages, currencies = transformer.transform()
+
+        logger.info("Transformation Completed")
+        return countries, regions, languages, currencies
+    
+    def load(self, countries, regions, languages, currencies):
+        logger.info("Starting Loading data in DB")
+
+        db = DatabaseManager()
+        db.create_tables()
+        db.load_dataframe(countries, "countries")
+        db.load_dataframe(regions, "regions")
+        db.load_dataframe(languages, "languages")
+        db.load_dataframe(currencies, "currencies")
+
+        db.close()
+
+        logger.info("Loading Data completed")
+
+    def run(self):
+        logger.info("ETL pipeline started")
+        self.extract()
+        df = self.validate()
+        countries, regions, languages, currencies = self.transform(df)
+        self.load(countries, regions, languages, currencies)
+        logger.info("ETL pipeline finished successfully")
+
--- main.py
+++ main.py
@@ -1,56 +1,10 @@
-import glob
-import os
-from app.ingestion.api_client import APIClient
-from app.processing.validator import DataValidator
-from app.processing.cleaner import DataCleaner
-from app.processing.transformer import DataTransformer
-from app.storage.database import DatabaseManager
+from app.processing.etl_pipeline import ETLPipeline
 
-def run_ingestion():
+def main():
     url = "https://restcountries.com/v3.1/all"
-    client = APIClient(url)
+    pipeline = ETLPipeline(url)
+    pipeline.run()
 
-    data = client.fetch_data()
-    client.save_raw_data(data)
-
-def run_validation():
-    latest_file = max(glob.glob("data/raw/*.json"), key=os.path.getctime)
-    validator = DataValidator(latest_file)
-    validator.load_raw_data()
-    validator.convert_to_dataframe()
-    validator.generate_schema_report()
-    df = validator.df
-    return df
-
-def run_cleaning(df):
-    cleaner = DataCleaner(df)
-    clean_df = cleaner.clean()
-    clean_df.to_csv("data/processed/clean_countries.csv", index=False)
-    return clean_df
-
-def run_transformation(clean_df):
-    transformer = DataTransformer(clean_df)
-    countries, regions, languages, currencies = transformer.transform()
-    countries.to_csv("data/processed/countries.csv", index=False)
-    regions.to_csv("data/processed/regions.csv", index=False)
-    languages.to_csv("data/processed/languages.csv", index=False)
-    currencies.to_csv("data/processed/currencies.csv", index=False)
-
-    return countries, regions, languages, currencies
-
-def run_loading(countries, regions, languages, currencies):
-    db = DatabaseManager()
-    db.create_tables()
-    db.load_dataframe(countries, "countries")
-    db.load_dataframe(regions, "regions")
-    db.load_dataframe(languages, "languages")
-    db.load_dataframe(currencies, "currencies")
-    db.close()
-    
 
 if __name__ == "__main__":
-    run_ingestion()
-    df= run_validation()
-    clean_df = run_cleaning(df)
-    countries, regions, languages, currencies = run_transformation(clean_df)
-    run_loading(countries, regions, languages, currencies)
+    main()
